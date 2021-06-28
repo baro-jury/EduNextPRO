d6459ebe834c4d023323bdf27b6ea02263a22521
baro-jury <84394521+baro-jury@users.noreply.github.com>
2021-06-29 01:35:11 +0700

    Delete AssignmentPRO/AssignmentPRO directory

--- AssignmentPRO/AssignmentPRO/src/IPet.java
+++
@@ -1,7 +0,0 @@
-public interface IPet<E> {
-    public void listAll();
-    public void addPet(E dog);
-    public void removePet(String id);
-    public void saveFile();
-    public void updateById(String id);
-}
