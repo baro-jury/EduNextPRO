8c6a52433e9bd4ffe5653e4da17a0feff0155ae8
baro-jury <84394521+baro-jury@users.noreply.github.com>
2021-07-10 18:36:13 +0700

    Delete EduNextPRO directory

--- EduNextPRO/src/IPet.java
+++
@@ -1,7 +0,0 @@
-public interface IPet<E> {
-    public void listAll();
-    public void addPet();
-    public void removePet(String id);
-    public void saveFile();
-    public void updateById(String id);
-}
